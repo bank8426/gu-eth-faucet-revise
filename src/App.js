ad5c115e5ad6c68c69d2a20ed3b20824c037039f
bank8426 <bank_55555@hotmail.com>
2019-08-20 12:28:17 +0700

    show user public address and current network

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Web3 from 'web3'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function getNetworkName (id){
+  switch(id){
+    case 1: return "Main net"
+    case 3: return "Ropsten testnet"
+    case 4: return "Rinkeby testnet"
+    case 5: return "Goerli testnet"
+    case 42: return "Kovan testnet"
+    default:
+        return "Unknown"
+  } 
+}
+
+class App extends Component {
+  state = {
+    userAddress: '',
+    currentNetwork: '',
+    currentNetworkId: '',
+  }
+
+  async componentDidMount(){
+    await window.ethereum.enable()
+    this.w3 = new Web3(window.ethereum)
+
+    const userAddress = (await this.w3.eth.getAccounts())[0]
+    this.setState({userAddress})
+
+    const currentNetworkId = await this.w3.eth.net.getId();
+    const currentNetwork = getNetworkName(currentNetworkId)
+    this.setState({currentNetwork,currentNetworkId})
+  }
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+      <p>Current Network: {this.state.currentNetwork}</p>
+      <p>Your Address: {this.state.userAddress}</p>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
